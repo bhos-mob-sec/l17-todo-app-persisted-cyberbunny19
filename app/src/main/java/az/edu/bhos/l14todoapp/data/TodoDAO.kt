249e1ae49865267864527de876a845a8535c14ff
cyberbunny19 <banu.orujova.std@bhos.edu.az>
2024-04-04 22:49:20 +0400

    “Hometask”

--- app/src/main/java/az/edu/bhos/l14todoapp/data/TodoDAO.kt
+++ app/src/main/java/az/edu/bhos/l14todoapp/data/TodoDAO.kt
@@ -1,8 +1,20 @@
 package az.edu.bhos.l14todoapp.data
 
-import androidx.room.Dao
+import androidx.room.*
+import az.edu.bhos.l14todoapp.data.dto.TodoLocalDto
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TodoDao {
-    // TODO: fill the required methods
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(todos: List<TodoLocalDto>)
+
+    @Delete
+    suspend fun delete(todo: TodoLocalDto)
+
+    @Query("SELECT * FROM todos")
+    fun getAllTodos(): Flow<List<TodoLocalDto>>
+
+    @Query("SELECT * FROM todos WHERE id = :id")
+    fun getTodoById(id: String): Flow<TodoLocalDto>
 }
